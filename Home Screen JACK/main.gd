22f9fbf0a6521b71f173e56ef20735e301252953
yungmicrowave <147096984+yungmicrowave@users.noreply.github.com>
2024-02-01 15:00:09 -0500

    I ADDED THE HOME SCREEN 🔥🔥🔥🔥🔥
    LETS GOOOO

---
+++ Home Screen JACK/main.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+
+
+func _on_button_2_pressed():get_tree().quit()
+
+
+
+func _on_button_pressed(): get_tree().change_scene_to_file("res://Home Screen JACK/quit_waffling.tscn")
+
+
+
+func _on_button_3_pressed():  get_tree().change_scene_to_file("res://Home Screen JACK/settings.tscn")
+
