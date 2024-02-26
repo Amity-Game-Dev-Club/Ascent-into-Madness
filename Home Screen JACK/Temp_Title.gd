9f1b0adb7451f101f5d376924959b021b52e0112
real <144968053+sillyfemboi@users.noreply.github.com>
2024-02-26 08:17:12 -0500

    failing to get godot working
    GG!

--- Home Screen JACK/Temp_Title.gd
+++ Home Screen JACK/Temp_Title.gd
@@ -6,7 +6,7 @@ func _on_button_2_pressed():get_tree().quit()
 
 
 
-func _on_button_pressed(): get_tree().change_scene_to_file("res://player.tscn")
+func _on_button_pressed(): get_tree().change_scene_to_file("res://world 2d.tscn")
 
 
 
