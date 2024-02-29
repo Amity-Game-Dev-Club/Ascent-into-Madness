17175af2e0e4dcc7ca95037f244a7d786d2402d2
yungmicrowave <147096984+yungmicrowave@users.noreply.github.com>
2024-02-29 14:59:23 -0500

    Organization

--- Home Screen JACK/quit_waffling.gd
+++ Home Screen JACK/quit_waffling.gd
@@ -1,5 +1,5 @@
 extends Node2D
 
-func _on_button_3_pressed(): get_tree().change_scene_to_file("res://Home Screen JACK/main.tscn")
+func _on_button_3_pressed(): get_tree().change_scene_to_file("res://Home Screen JACK/Temp_Title.tscn")
 
 
