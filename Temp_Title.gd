2d8a0d030e7140a94f4434b1722e5319f47c6bb6
real <144968053+sillyfemboi@users.noreply.github.com>
2024-02-29 16:41:32 -0500

    fixing bugs
    sorry abt this but its the only way i could get  the game to not have massive errors.
    also removed epty files and fixed filenames to make them show better

--- Temp_Title.gd
+++ Temp_Title.gd
@@ -7,7 +7,7 @@ func _on_button_2_pressed():get_tree().quit()
 
 #Make sure its going to the right place. ive had to fix this like  times 
 #now broken rn idk why trying to fix it currently -Emil 
-func _on_button_pressed(): get_tree().change_scene_to_file("res://quit_waffling.tscn")
+func _on_button_pressed(): get_tree().change_scene_to_file("res://Home Screen JACK/quit_waffling.tscn")
 
 
 
--- code for gun.gd
+++ code for gun.gd
@@ -1,18 +1,12 @@
 extends Sprite2D
+
 var gunPos = 1
 @export var mousePos = 1
-
 var mouse_pos = 1
-
-
 
 func _input(event):
 	if event is InputEventMouseMotion:
-		mouse_pos =   get_global_mouse_position() #  get_viewport().get_mouse_position()
+		mouse_pos =   get_global_mouse_position() 
 		look_at(Vector2(mouse_pos.x, mouse_pos.y))
-		
-
-
 # func _process(delta):
-	var mouse_pos = get_viewport().get_mouse_position()
-	
+
