3317bd6ac0ad755ad6a2b1d0d67b7c4f05918870
yungmicrowave <147096984+yungmicrowave@users.noreply.github.com>
2024-02-29 15:16:00 -0500

    dffrtefd

--- Home Screen JACK/Temp_Title.gd
+++ Home Screen JACK/Temp_Title.gd
@@ -5,7 +5,7 @@ extends Node2D
 func _on_button_2_pressed():get_tree().quit()
 
 
-
+#Make sure its going to the right place. ive had to fix this like 3 times 
 func _on_button_pressed(): get_tree().change_scene_to_file("res://Home Screen JACK/quit_waffling.tscn")
 
 
