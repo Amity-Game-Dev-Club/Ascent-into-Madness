0c34d893b29001338be85d9d7cbbc774bd3393f4
real <mineyminergaming@gmail.com>
2024-03-06 11:33:10 -0500

    cleaning up files
    publishing in order to see if other people have the same gdscript error as me. the code works at this point

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -1,5 +1,8 @@
 extends CharacterBody2D
 
+@onready var aimcast = $"gun sprite/RayCast2D"
+@onready var muzzle = $"gun sprite/muzzle"
+@onready var bullet = load("res://bullet.gd")
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
@@ -8,23 +11,26 @@ var doublejump = 1
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
-func _physics_process(delta):
+func _physics_process(delta: float) -> void:
 	# Add the gravity.
+	var direction = Input.get_axis("ui_left", "ui_right")
 	if not is_on_floor():
 		velocity.y += gravity * delta
 		if Input.is_action_just_pressed("ui_up") and doublejump > 0:
 			velocity.y = JUMP_VELOCITY
 			doublejump= doublejump - 1
+			
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		velocity.x = direction * SPEED * 1.2
 		doublejump = 1 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	$Camera2D/HUD/Label.text = "damage : %s" % [$"gun sprite".damage]
--- code for gun.gd
+++ code for gun.gd
@@ -1,12 +1,28 @@
 extends Sprite2D
-
+#useless vars that we wont change
+@onready var aimcast = $"aiming go brrr"
 var gunPos = 1
 @export var mousePos = 1
 var mouse_pos = 1
+@onready var muzzle = $"gun sprite/muzzle"
+@onready var bullet = preload("res://bullet.gd")
+#gun stats
+var damage = 25
+var firerate = 14 #fire rate is in rounds per second 
+var magsize = 30 
 
+
+#gun rotation
 func _input(event):
 	if event is InputEventMouseMotion:
 		mouse_pos =   get_global_mouse_position() 
 		look_at(Vector2(mouse_pos.x, mouse_pos.y))
-# func _process(delta):
 
+# right now im just going to try and use hitscan wepons for now. later i will fix this and mak it into projectile wepons
+func _physics_process(delta: float) -> void:
+	if aimcast.is_colliding():
+		if Input.is_action_pressed("mouseL"):
+			var b = bullet.instantiate()
+			muzzle.add_child(b)
+			b.look_at(aimcast.get_collision_point())
+			b.shoot = true
